8af2e85b2927fa233e5289efe45ce6c07ae92091
Fabien Delorme <delorme@cril.fr>
2017-12-01 10:49:04 +0100

    modified main executable's usage

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
 	"runtime/debug"
 	"sort"
@@ -16,47 +15,84 @@ import (
 func main() {
 	// defer profile.Start().Stop()
 	debug.SetGCPercent(300)
-	if len(os.Args) > 2 {
-		fmt.Fprintf(os.Stderr, "Syntax : %s [file.cnf|file.bf]\n", os.Args[0])
+	var (
+		verbose bool
+		count   bool
+	)
+	flag.BoolVar(&verbose, "verbose", false, "sets verbose mode on")
+	flag.BoolVar(&count, "count", false, "rather than solving the problem, counts the number of models it accepts")
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		fmt.Fprintf(os.Stderr, "Syntax : %s [options] (file.cnf|file.bf|file.opb)\n", os.Args[0])
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	f := os.Stdin
-	if len(os.Args) == 2 {
-		var err error
-		f, err = os.Open(os.Args[1])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Could not open %s: %v\n", os.Args[1], err.Error())
-			os.Exit(1)
-		}
-		defer f.Close()
-	}
-	if err := parseAndSolve(f); err != nil {
-		fmt.Fprintf(os.Stdout, "could not solve problem: %v\n", err)
+	if pb, err := parse(flag.Args()[0]); err != nil {
+		fmt.Fprintf(os.Stdout, "could not parse problem: %v\n", err)
 		os.Exit(1)
+	} else if count {
+		countModels(pb, verbose)
+	} else {
+		solve(pb, verbose)
 	}
 }
 
-func parseAndSolve(r io.Reader) error {
-	content, err := ioutil.ReadAll(r)
+func countModels(pb *solver.Problem, verbose bool) {
+	s := solver.New(pb)
+	if verbose {
+		fmt.Printf("c ======================================================================================\n")
+		fmt.Printf("c | Number of clauses   : %9d                                                    |\n", len(pb.Clauses))
+		fmt.Printf("c | Number of variables : %9d                                                    |\n", pb.NbVars)
+		s.Verbose = true
+	}
+	fmt.Println(s.CountModels())
+}
+
+func solve(pb *solver.Problem, verbose bool) {
+	s := solver.New(pb)
+	if verbose {
+		fmt.Printf("c ======================================================================================\n")
+		fmt.Printf("c | Number of clauses   : %9d                                                    |\n", len(pb.Clauses))
+		fmt.Printf("c | Number of variables : %9d                                                    |\n", pb.NbVars)
+		s.Verbose = true
+	}
+	s.Minimize()
+	if verbose {
+		fmt.Printf("c nb conflicts: %d\nc nb restarts: %d\nc nb decisions: %d\n", s.Stats.NbConflicts, s.Stats.NbRestarts, s.Stats.NbDecisions)
+		fmt.Printf("c nb unit learned: %d\nc nb binary learned: %d\nc nb learned: %d\n", s.Stats.NbUnitLearned, s.Stats.NbBinaryLearned, s.Stats.NbLearned)
+		fmt.Printf("c nb clauses deleted: %d\n", s.Stats.NbDeleted)
+	}
+	s.OutputModel()
+}
+
+func parse(path string) (pb *solver.Problem, err error) {
+	f, err := os.Open(path)
 	if err != nil {
-		return fmt.Errorf("could not read data: %v", err)
+		return nil, fmt.Errorf("could not open %q: %v", path, err)
 	}
-	r2 := strings.NewReader(string(content))
-	f, errBF := bf.Parse(r2)
-	if errBF == nil {
-		return solveBF(f)
+	defer f.Close()
+	if strings.HasSuffix(path, ".bf") {
+		_, err := bf.Parse(f)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse %q: %v", path, err)
+		}
+		panic("not yet implemented")
 	}
-	r2 = strings.NewReader(string(content))
-	pb, errCNF := solver.ParseCNF(r2)
-	if errCNF == nil {
-		return solveCNF(pb)
+	if strings.HasSuffix(path, ".cnf") {
+		pb, err := solver.ParseCNF(f)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse DIMACS file %q: %v", path, err)
+		}
+		return pb, nil
 	}
-	r2 = strings.NewReader(string(content))
-	pb, errOPB := solver.ParseOPB(r2)
-	if errOPB != nil {
-		return fmt.Errorf("could not parse content as DIMACS (%v), as boolean formula (%v), nor as a pseudo-boolean problem (%v)", errCNF, errBF, errOPB)
+	if strings.HasSuffix(path, ".opb") {
+		pb, err := solver.ParseOPB(f)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse OPB file %q: %v", path, err)
+		}
+		return pb, nil
 	}
-	return minimize(pb)
+	return nil, fmt.Errorf("invalid file format for %q", path)
 }
 
 func minimize(pb *solver.Problem) error {
@@ -84,16 +120,6 @@ func solveCNF(pb *solver.Problem) error {
 	fmt.Printf("c nb unit learned: %d\nc nb binary learned: %d\nc nb learned: %d\n", s.Stats.NbUnitLearned, s.Stats.NbBinaryLearned, s.Stats.NbLearned)
 	fmt.Printf("c nb clauses deleted: %d\n", s.Stats.NbDeleted)
 	s.OutputModel()
-	return nil
-}
-
-func countModels(pb *solver.Problem) error {
-	fmt.Printf("c ======================================================================================\n")
-	fmt.Printf("c | Number of clauses   : %9d                                                    |\n", len(pb.Clauses))
-	fmt.Printf("c | Number of variables : %9d                                                    |\n", pb.NbVars)
-	s := solver.New(pb)
-	s.Verbose = true
-	fmt.Printf("%d models found\n", s.CountModels())
 	return nil
 }
 
