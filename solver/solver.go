8b83715b435a37402f292b1b1afe7faa03d897a3
Fabien Delorme <fabien.delorme@gmail.com>
2023-03-06 15:11:48 +0100

    removing useless affectations

--- solver/solver.go
+++ solver/solver.go
@@ -509,7 +509,7 @@ func (s *Solver) Enumerate(models chan []bool, stop chan struct{}) int {
 	}
 	s.lastModel = make(Model, len(s.model))
 	nb := 0
-	lit := s.chooseLit()
+	var lit Lit
 	var lvl decLevel
 	for s.status != Unsat {
 		for s.status == Indet {
@@ -579,7 +579,7 @@ func (s *Solver) CountModels() int {
 		}()
 	}
 	nb := 0
-	lit := s.chooseLit()
+	var lit Lit
 	var lvl decLevel
 	for s.status != Unsat {
 		for s.status == Indet {
