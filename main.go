d7439e08e8004a3a569c8b3afd0b64599a45eafc
Fabien Delorme <fabien.delorme@gmail.com>
2023-03-08 13:02:51 +0100

    calling correct printing function

--- main.go
+++ main.go
@@ -31,13 +31,13 @@ func main() {
 	flag.BoolVar(&help, "help", false, "displays help")
 	flag.Parse()
 	if !help && len(flag.Args()) != 1 {
-		fmt.Printf(helpString)
+		fmt.Print(helpString)
 		fmt.Fprintf(os.Stderr, "Syntax : %s [options] (file.cnf|file.wcnf|file.bf|file.opb)\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 	if help {
-		fmt.Printf(helpString)
+		fmt.Print(helpString)
 		fmt.Printf("Syntax : %s [options] (file.cnf|file.wcnf|file.bf|file.opb)\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(0)
